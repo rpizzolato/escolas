9542fe28826588e7be858a22de2776dc22bb9880
Rodrigo <rpessolato@gmail.com>
2020-11-25 22:30:56 -0300

    Added lat/lng info for pos fix and popup pos fix

--- src/assets/mapMarkerIcon.ts
+++ src/assets/mapMarkerIcon.ts
@@ -5,7 +5,8 @@ import mapMarkerIcon from '../images/marker-icon.png';
 const mapMarker = Leaflet.icon({
   iconUrl: mapMarkerIcon,
   iconSize: [30, 47],
-  
+  iconAnchor: [15, 47],
+  popupAnchor: [0, -50]
 })
 
 export default mapMarker;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 
 import '../styles/pages/main.css';
 import 'leaflet/dist/leaflet.css';
@@ -7,9 +7,35 @@ import 'leaflet/dist/leaflet.css';
 import mapMarkerIcon from '../assets/mapMarkerIcon';
 
 import data from '../data/escolas.json';
+import { LeafletMouseEvent } from 'leaflet';
 
 export default function Main() {
-  console.log(data.escolas);
+
+  function AddMarkerToClick() {
+    const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+    const map = useMapEvents({
+      click(event: LeafletMouseEvent) {
+        const { lat, lng } = event.latlng;
+        setPosition({
+          latitude: lat,
+          longitude: lng,
+        });
+        console.log(position);
+      }
+    })
+
+    return(
+      position.latitude !== 0 ?
+      (
+        <Marker 
+          position={[position.latitude, position.longitude]}
+          icon={mapMarkerIcon}/>
+      ) :
+      null
+    );
+  }
+
   return (
 
     <div id="main-container">
@@ -19,7 +45,7 @@ export default function Main() {
 
       <MapContainer
         center={[-22.7244976,-47.6352641]}
-        zoom={13}
+        zoom={12.5}
         style={{ width: '100%', height: '100%' }}>
 
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
@@ -30,17 +56,18 @@ export default function Main() {
                 icon={mapMarkerIcon}
                 position={[escola.latitude,escola.longitude]}
                 key={escola.id}>
-                <Popup>
-                  {escola.nome}<br />
+                <Popup className="map-popup">
+                  <strong>{escola.nome}</strong><br />
                   {escola.endereco}<br />
                   {escola.bairro}<br />
-                  Telefone: {escola.telefone}<br />
+                  Telefone: <strong>{escola.telefone}</strong><br />
                   Região: {escola.regiao}<br />
                 </Popup>
               </Marker>
             )
         })}
 
+        <AddMarkerToClick/>
 
       </MapContainer>
     </div>
