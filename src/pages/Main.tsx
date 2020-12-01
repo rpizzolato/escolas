393bf12f9d2202914c8e1873bb2e37082d315ee7
Rodrigo <rpessolato@gmail.com>
2020-12-01 14:44:06 -0300

    listing suggestions, but with no style

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -15,6 +15,14 @@ export default function Main() {
   const [value, setValue] = useState("");
   const [suggestions, setSuggestions] = useState<string[]>([]);
 
+  const lowerCasedSchoolsName = data.escolas.map(escola => escola.nome.toLowerCase());
+
+  console.log(lowerCasedSchoolsName);
+
+  function getSuggestions(value: string): string[] {
+    return lowerCasedSchoolsName.filter(school => school.includes(value.trim().toLowerCase()));
+  }
+
   function handleFilterSchools(event: React.FormEvent<HTMLInputElement>) {
     const search = event.currentTarget.value;
 
@@ -31,7 +39,28 @@ export default function Main() {
 
     <div id="main-container">
       <aside>
-        <input type="text" placeholder="Entre com o nome da escola" onChange={handleFilterSchools}/>
+        {/* <input type="text" placeholder="Entre com o nome da escola" onChange={handleFilterSchools}/> */}
+        <AutoSuggest
+          suggestions={suggestions}
+          onSuggestionsClearRequested={() => setSuggestions([])}
+          onSuggestionsFetchRequested={({ value }) => {
+            setValue(value);
+            setSuggestions(getSuggestions(value));
+          }}
+          onSuggestionSelected={(_, { suggestionValue }) =>
+            console.log("Selected: " + suggestionValue)
+          }
+          getSuggestionValue={suggestion => suggestion}
+          renderSuggestion={suggestion => <span>{suggestion}</span>}
+          inputProps={{
+            placeholder: "Type 'c'",
+            value: value,
+            onChange: (_, { newValue, method }) => {
+              setValue(newValue);
+            }
+          }}
+          highlightFirstSuggestion={true}
+        />
       </aside>
 
 
